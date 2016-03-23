8e19ff18ad7d1e434d9e7b8fcf845a09e852e3d2
Rúben Azevedo <ruben.azevedo@farfetch.com>
2016-03-23 20:51:04 +0000

    Removed auto

--- js/vanishow.js
+++ js/vanishow.js
@@ -1,46 +1,28 @@
 function VANISHOW(selector, settings) {
 	var _defaults = {
-		auto: false,
-		arrows: false
+		arrows: false,
+		arrowsText: false
 	};
 
 	this.settings = Object.assign({}, _defaults, settings);
 
 	this.selector = selector;
+
 	this.mainEl = document.querySelector(selector);
 	this.childs = document.querySelectorAll(this.selector + ' img');
 
 	this.current = 0;
 	this.eachWidth = this.childs[0].offsetWidth;
-
 	this.slideLength = this.childs.length;
+	
 	this.start();
 }
 
 VANISHOW.prototype = {
 	start: function() {
-		if(this.settings.auto) {
-			this.auto();
-		}
 		if(this.settings.arrows) {
 			this.arrows();
 		}
-	},
-
-	auto: function() {
-		//Helper variable
-        var _this = this;
-
-        if(_this.settings.auto && typeof _this.settings.auto !== 'number' ) _this.settings.auto = 3000;
-
-        //He will loop here
-        setTimeout(function() {
-            //Calls the function SlideTo and sends the next slide
-            _this.slideTo(_this.current + 1);
-            //Calls this function again in order to loop
-            _this.auto();
-            //Loop in X in X seconds
-        }, _this.settings.auto);
 	},
 
 	arrows: function() {
@@ -82,8 +64,6 @@ VANISHOW.prototype = {
 
 window.onload = function () {
 	var slide = new VANISHOW('#vanishow', {
-		auto: false,
-
 		arrows: true,
 		arrowsText: ['Prev', 'Next']
 	});
